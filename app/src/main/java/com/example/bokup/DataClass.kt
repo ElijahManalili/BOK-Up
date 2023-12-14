baeefb1aa2d7efd55ab8c15ac1d207bb038f1a40
ElijahManalili <elijahklyde.manalili.cics@ust.edu.ph>
2023-12-14 22:48:55 +0800

    BOK ME DADDY PLS

--- app/src/main/java/com/example/bokup/DataClass.kt
+++ app/src/main/java/com/example/bokup/DataClass.kt
@@ -1,3 +1,3 @@
 package com.example.bokup
 
-data class DataClass(var timeCal : String ?= null, var cal : String ?= null,)
+data class DataClass(var timeCal : String ?= null, var cal : String ?= null)
